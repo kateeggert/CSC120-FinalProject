068bf6928aad384098f3cbbb90ac1c3231f74778
kateeggert <keggert@smith.edu>
2025-12-09 10:26:19 -0500

    12/9 updated game loop

--- gameLoop.java
+++ gameLoop.java
@@ -19,39 +19,50 @@ public class gameLoop {
         System.out.println("******************");
 
         // Instructions
-        System.out.println("Navigate the maze to find the triwizard cup! Watch out for obstacles and the shifting walls.");
+        System.out.println("Navigate the maze to find the triwizard cup! Watch out for obstacles and beware of the walls.");
 
         // Generate instances of the classes
         player user = new player();
         maze triwizardMaze = new maze();
 
-        // The do...while structure means we execute the body of the loop once before checking the stopping condition
+        // Runs the game until the user dies or is at the end
         do {
-            // ************************************************
-            // The stuff that happens in your game will go here
-            //  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓
+
             System.out.println("You are still playing. Follow the instructions if you want to win/lose...");
             userResponse = userInput.nextLine().toUpperCase();
-            // If the user inputs a direction, move in that direction. If not, nothing will happen.
+
+            // If the user inputs a direction, move in that direction. If the direction is invalid, the user will lose health.
             triwizardMaze.move(userResponse);
-            // ***********************************************************************
-            // And as the player interacts, you'll check to see if the game should end
-            //  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓  ↓
+
+            // If there is a sphinx in the user's location, run the riddle and user deduct health accordingly.
+            if (triwizardMaze.isSphinx()) {
+                int deductHealth;
+                sphinx mySpinx = new sphinx(0, 0, userResponse, userResponse, userResponse);
+                deductHealth = mySpinx.askRiddle();
+                for (int i = 0; i < deductHealth; i++) {
+                    user.deductHealth();
+                }
+            }
 
             // If the user runs out of health, game stops - EDIT: add other ways that the game can be finished
             if (!user.hasHealth()) {
                 stillPlaying = false;
             }
+
+            if (triwizardMaze.playerAtEnd()) {
+                stillPlaying = false;
+            }
+
         } while (stillPlaying);
 
         // Tidy up
         userInput.close();
 
         // Once you exit the loop, you may need to deal with various possible stopping conditions
-        if (userResponse.equals("WIN")) {
-            System.out.println("Yay, you won!");
-        } else { // userResponse.equals("LOSE")
-            System.out.println("Better luck next time.");
+        if (triwizardMaze.playerAtEnd()) {
+            System.out.println("Yay, you won the Triwizard Cup! Don't run into You Know Who anytime soon...");
+        } else {
+            System.out.println("Better luck next time. At least you didn't find You Know Who");
         }
 
     }
